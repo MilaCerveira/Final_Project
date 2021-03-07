026e27fa50841b56225f99bed1ce83adad70b955
Andrej Mihalylovics <andrej.mihalylovics@gmail.com>
2021-03-07 11:41:26 +0000

    created mapping questions by language

--- server/src/main/java/com/example/language_game/controllers/QuestionController.java
+++ server/src/main/java/com/example/language_game/controllers/QuestionController.java
@@ -8,6 +8,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
@@ -19,6 +20,17 @@ public class QuestionController {
     QuestionRepository questionRepository;
 
     @GetMapping("/questions")
-    public ResponseEntity<List<Question>> getAllQuestions(){
-        return new ResponseEntity<>(questionRepository.findAll(), HttpStatus.OK);}
+    public ResponseEntity<List<Question>> getAllQuestions() {
+        return new ResponseEntity<>(questionRepository.findAll(), HttpStatus.OK);
+    }
+
+
+    @GetMapping("/questions/")
+    public ResponseEntity<List<Question>> findBylanguage(
+            @RequestParam(name = "language") String language) {
+        return new ResponseEntity<>(questionRepository.findBylanguage(language), HttpStatus.OK);
+
+
+    }
 }
+
