aa6341c4ffed13e780cd1d6516dee84feb9c6f5e
milacerveira <milascerveira@gmail.com>
2021-03-08 19:11:04 +0000

    passing props working question and answers displaying buttons ok

--- client/src/components/Question.js
+++ client/src/components/Question.js
@@ -90,25 +90,7 @@ return (
         }
         </Grid.Column>
         </Grid.Row>
-        <Grid.Row columns={2}>
-    <Grid.Column>
-        {//questions[activeQuestion].answers[0]
-        }
-    </Grid.Column>
-    <Grid.Column>
-        
-        {/* {questions[activeQuestion].answers[1].answerbody} */}
-        {questions[activeQuestion].body}
-        </Grid.Column>
-        </Grid.Row>
-        <Grid.Row columns={2}>
-        <Grid.Column>
-        {questions[activeQuestion].body}
-        </Grid.Column>
-        <Grid.Column>
-        {questions[activeQuestion].body}
-        </Grid.Column>
-        </Grid.Row>
+       
         </Grid>
         <div>Question {0 + 1} out of {questions.length}</div>
         </div> 
